cb1127d9d9c2bfab174ff0b1b92a59e020c8e731
Tim <67833362+swaiist@users.noreply.github.com>
2024-06-11 23:29:40 +0200

    First version added

---
+++ disable-windows-recall.bat
@@ -0,0 +1,14 @@
+@echo off
+reg query "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsAI" >nul 2>&1
+if %ERRORLEVEL% neq 0 (
+    reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsAI" >nul
+)
+
+reg add "HKEY_CURRENT_USER\Software\Policies\Microsoft\Windows\WindowsAI" /v DisableAIDataAnalysis /t REG_DWORD /d 1 /f
+
+shutdown /r /t 0
+
+
+
+⚠️#Attention: Your PC will restart afterwards
+⚠️#Attention: Do it only at your own risk
